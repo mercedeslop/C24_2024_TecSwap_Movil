5f7867cd21f7c7ea176e912311931e9a783fc54a
mercedeslop <jenifersasa2004@gmail.com>
2024-06-11 11:22:31 -0500

    Inicio de Sesion con google

--- app/src/main/java/com/jennyfer/sapallanay/tecswap/ui/activities/MainActivity2.kt
+++ app/src/main/java/com/jennyfer/sapallanay/tecswap/ui/activities/MainActivity2.kt
@@ -1,12 +1,11 @@
 package com.jennyfer.sapallanay.tecswap.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.jennyfer.sapallanay.tecswap.R
 import com.jennyfer.sapallanay.tecswap.databinding.ActivityMain2Binding
 import com.jennyfer.sapallanay.tecswap.ui.fragments.ExplorarFragment
@@ -15,16 +14,17 @@ import com.jennyfer.sapallanay.tecswap.ui.fragments.PerfilFragment
 import com.jennyfer.sapallanay.tecswap.ui.fragments.ProductosFragment
 import com.jennyfer.sapallanay.tecswap.ui.fragments.TruequesFragment
 
-class MainActivity2 : AppCompatActivity() {
-    // Con esto nos vinculamos con la vista
+class MainActivity2 : AppCompatActivity(), PerfilFragment.SignOutCallback {
     private lateinit var binding: ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Para manejar los fragmentos
         binding.bottomNavMenu.setOnItemSelectedListener { menuItem ->
-            when(menuItem.itemId){
+            when (menuItem.itemId) {
                 R.id.mExplora -> {
                     replaceFragment(ExplorarFragment())
                     true
@@ -53,5 +53,12 @@ class MainActivity2 : AppCompatActivity() {
 
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().replace(R.id.frameContainer, fragment).commit()
+    }
+
+    override fun signOut() {
+        Firebase.auth.signOut()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- app/src/main/java/com/jennyfer/sapallanay/tecswap/ui/fragments/PerfilFragment.kt
+++ app/src/main/java/com/jennyfer/sapallanay/tecswap/ui/fragments/PerfilFragment.kt
@@ -1,22 +1,51 @@
 package com.jennyfer.sapallanay.tecswap.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.jennyfer.sapallanay.tecswap.R
+import com.jennyfer.sapallanay.tecswap.databinding.FragmentPerfilBinding
 
 
 class PerfilFragment : Fragment() {
 
+    private var _binding: FragmentPerfilBinding? = null
+    private val binding get() = _binding!!
+
+    private var callback: SignOutCallback? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is SignOutCallback) {
+            callback = context
+        } else {
+            throw RuntimeException("$context must implement SignOutCallback")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false)
+    ): View {
+        _binding = FragmentPerfilBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnCerrarSesion.setOnClickListener {
+            callback?.signOut()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    interface SignOutCallback {
+        fun signOut()
+    }
 }
